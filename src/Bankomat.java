3f6c9578b4492f59b9f435a869c45365ec7460b2
Luca <luca.grabherr@student.htldornbirn.at>
2022-03-16 16:32:55 +0100

    bankautomat

--- src/Bankomat.java
+++ src/Bankomat.java
@@ -2,8 +2,51 @@ import java.util.Scanner;
 
 public class Bankomat {
     public static void main(String[] args) {
+        boolean isFinished = true;
 
-        System.out.println("hello");
+
         Scanner scanner = new Scanner(System.in);
+
+
+        int Kontostand = 0;
+
+
+        while (isFinished){
+            int selection = scanner.nextInt();
+            if (selection == 1){
+                System.out.println("Geben Sie den Betrag ein den Sie gerne einzahlen möchten:");
+                selection = scanner.nextInt();
+                Kontostand += selection;
+                System.out.println("Sie haben " + Kontostand + " eingezahlt");
+
+
+            }
+
+            if(selection == 2){
+                System.out.println("Geben Sie den Betrag ein den Sie gerne abheben möchten:");
+                selection = scanner.nextInt();
+                Kontostand -= selection;
+                System.out.println("Sie haben " + selection + " abgehoben");
+
+            } else if (Kontostand<0){
+                System.out.println("error");
+            }
+
+            if(selection == 3){
+                System.out.println("Der Kontostand beträgt " + Kontostand);
+            }
+
+            if(selection == 4){
+                isFinished = false;
+                System.out.println("Machine is stopping");
+
+
+            }
+        }
+
     }
+
+
+
 }
+
