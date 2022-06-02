2c2f21c30660ce325a60935bbc2f0288955415d5
Luca <luca.grabherr@student.htldornbirn.at>
2022-06-02 16:07:14 +0200

    Test; Test2

---
+++ src/Test.java
@@ -0,0 +1,28 @@
+
+import java.util.Random;
+
+public class Test {
+
+    public static void main(String[] args) {
+        int randomNumber1;
+        int randomNumber2;
+
+        Random random = new Random();
+        randomNumber1 = random.nextInt(1001);
+        randomNumber2 = random.nextInt(1001);
+
+        if (randomNumber1 > randomNumber2) {
+            for (int i = randomNumber1; i >= randomNumber2; i--) {
+                System.out.println(randomNumber1);
+                randomNumber1 = randomNumber1 - 1;
+            }
+        } else if (randomNumber2 > randomNumber1) {
+            for (int i = randomNumber2; i >= randomNumber1; i--) {
+                System.out.println(randomNumber2);
+                randomNumber2 = randomNumber2 - 1;
+            }
+        }
+    }
+}
+
+
---
+++ src/Test2.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+public class Test2 {
+
+    public static void main(String[] args) {
+        boolean isFinished = true;
+        int result = 0;
+
+        while (isFinished){
+
+            int randomnumber;
+            Random random = new Random();
+            randomnumber = random.nextInt(11);
+
+            result = result + randomnumber;
+
+            if (randomnumber == 5){
+                break;
+            }
+
+        }
+        System.out.println(result);
+    }
+}
