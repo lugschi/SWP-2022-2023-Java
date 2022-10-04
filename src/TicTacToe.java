55ff558d921331e968e727a292f8884007678259
Luca <luca.grabherr@student.htldornbirn.at>
2022-10-04 11:24:59 +0200

    Calendar TicTacToe

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,10 +1,82 @@
 import java.util.*;
+import java.util.Scanner;
 
 public class TicTacToe {
-    public static void main(String[] args) {
-            String [] board;
-            String [] move;
+    public static String main(String[] args) {
 
+
+        String[] board = new String[0];
+        String turn = null;
+
+
+        for (int a = 0; a < 8; a++) {
+            String line = null;
+
+            switch (a) {
+                case 0:
+                    line = board[0] + board[1] + board[2];
+                    break;
+                case 1:
+                    line = board[3] + board[4] + board[5];
+                    break;
+                case 2:
+                    line = board[6] + board[7] + board[8];
+                    break;
+                case 3:
+                    line = board[0] + board[3] + board[6];
+                    break;
+                case 4:
+                    line = board[1] + board[4] + board[7];
+                    break;
+                case 5:
+                    line = board[2] + board[5] + board[8];
+                    break;
+                case 6:
+                    line = board[0] + board[4] + board[8];
+                    break;
+                case 7:
+                    line = board[2] + board[4] + board[6];
+                    break;
+            }
+
+            if (line.equals("XXX")) {
+                return "X";
+            }
+
+            // For O winner
+            else if (line.equals("OOO")) {
+                return "O";
+            }
+        }
+
+        for (int a = 0; a < 9; a++) {
+            if (Arrays.asList(board).contains(
+                    String.valueOf(a + 1))) {
+                break;
+            } else if (a == 8) {
+                return "draw";
+            }
+        }
+
+        // To enter the X Or O at the exact place on board.
+        System.out.println(
+                turn + "'s turn; enter a slot number to place "
+                        + turn + " in:");
+
+
+
+
+
+
+        System.out.println(
+                "X will play first. Enter a slot number to place X in:");
+
+        return turn;
     }
-
 }
+
+
+
+
+
+
