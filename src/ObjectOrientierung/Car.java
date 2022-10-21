1c1fdee97dfb4a2bffc71894f4047d02a337e1e0
Luca <luca.grabherr@student.htldornbirn.at>
2022-10-21 14:44:40 +0200

    ObjectO01

--- src/ObjectOrientierung/Car.java
+++ src/ObjectOrientierung/Car.java
@@ -2,7 +2,48 @@ package ObjectOrientierung;
 
 public class Car {
     public int fuelConsumption;
+    public int fuelAmount;
     public String brand;
     public String serialNumber;
     private String color;
+    public int range;
+
+    public Car(int fuelConsumption, String brand, String serialNumber) {
+        this.fuelConsumption = fuelConsumption;
+        this.brand = brand;
+        this.serialNumber = serialNumber;
+
+    }
+
+    // Methode
+    public void drive() {
+        this.fuelAmount = this.fuelAmount - fuelConsumption;
+        System.out.println("I am driving");
+    }
+
+    public void breaks() {
+        System.out.println("Ich bremse");
+    }
+
+    public void turboBoost() {
+        if (this.fuelAmount >= this.fuelAmount * 0.1) {
+            System.out.println("SuperBoostMode");
+        } else {
+            System.out.println("Not enough fuel to go Super Boost");
+        }
+    }
+
+    public void getRemainingRange() {
+
+        range = fuelAmount / fuelConsumption;
+        System.out.println(range +"km");
+    }
+
+    public void honk(int amountOfRepetitions) {
+        for (int i = 0; i < amountOfRepetitions; i++) {
+            System.out.print("Tuut");
+        }
+    }
+
+
 }
--- src/ObjectOrientierung/ObjectO01.java
+++ src/ObjectOrientierung/ObjectO01.java
@@ -2,17 +2,36 @@ package ObjectOrientierung;
 
 public class ObjectO01 {
     public static void main(String[] args) {
+        /*
         Car c1 = new Car();
         c1.brand = "Audi";
-        c1.fuelConsumption = 6;
-        c1.serialNumber = "A420";
+        c1.fuelConsumption = 7;
+        c1.serialNumber = "A1234";
+        c1.fuelAmount = 70;
 
         Car c2 = new Car();
-        c2.brand = "BMW";
-        c2.fuelConsumption = 4;
-        c2.serialNumber = "A069";
+        c2.brand = "Mercedes";
+        c2.fuelConsumption = 6;
+        c2.serialNumber = "M1234";
+        c2.fuelAmount = 20;
 
-        System.out.println(c1.brand);
-        System.out.println(c2.brand);
+        System.out.println(c2.fuelAmount);
+        c2.drive();
+        System.out.println(c2.fuelAmount);
+
+        c1.breaks();
+        c1.turboBoost();
+        c1.getRemainingRange();
+        c1.honk(3);
+*/
+
+        Car c3 = new Car(17, "Opel", "1234O");
+
+        System.out.println(c3.serialNumber);
+
+
+
+
+
     }
 }
