461c432c26c924146c5d9dd617d80f10228fe05c
Luca <luca.grabherr@student.htldornbirn.at>
2023-03-24 14:34:43 +0100

    Übung 04 Fertig Anfang Übung 05

--- src/GamesOO/src/at/grl/games/firstgame/ObjectsGames.java
+++ src/GamesOO/src/at/grl/games/firstgame/ObjectsGames.java
@@ -3,14 +3,8 @@ package at.grl.games.firstgame;
 import org.newdawn.slick.*;
 
 public class ObjectsGames extends BasicGame {
-    private float xRec;
-    private float yRec;
-    private float xOv;
-    private float yOv;
-    private float speed;
-    private boolean left;
-    private float xCi;
-    private float yCi;
+    private Rectangle rectangle;
+    private Rectangle rectangle2;
 
 
 
@@ -20,67 +14,22 @@ public class ObjectsGames extends BasicGame {
 
     @Override
     public void init(GameContainer gameContainer) throws SlickException {
-        this.xRec = 100;
-        this.yRec = 100;
-        this.xOv = 0;
-        this.yOv = 0;
-        this.speed = 5.0F;
-        this.left = false;
-        this.xCi = 0;
-        this.yCi = 0;
+        this.rectangle = new Rectangle(100, 100, 5);
+        this.rectangle2 = new Rectangle(5, 400, 10);
 
     }
 
     @Override
     public void update(GameContainer gameContainer, int delta) throws SlickException {
-
-        //Rectangle circle
-        if (this.xRec <=500 && this.yRec <=100){
-            this.xRec += (float)delta/this.speed;
-        } else if (this.xRec >= 500 && this.yRec <=400){
-            this.yRec += (float)delta/this.speed;
-        } else if (this.xRec >= 100 && this.yRec >= 400){
-            this.xRec -= (float)delta/this.speed;
-        } else if (this.xRec <=100 && this.yRec >=100){
-            this.yRec -= (float)delta/this.speed;
-        }
-
-
-        //x Oval
-        if ((int)xOv ==700){
-            this.left = true;
-        } else if ((int)xOv==0){
-            this.left = false;
-        }
-        if (left){
-            this.xOv -= (float)delta/this.speed;
-        } else if (!left){
-            this.xOv += (float)delta/this.speed;
-        }
-
-        //y Circle
-        if ((int)yCi ==500){
-            this.left = true;
-        } else if ((int)yCi==0){
-            this.left = false;
-        }
-        if (left){
-            this.yCi -= (float)delta/this.speed;
-        } else if (!left){
-            this.yCi += (float)delta/this.speed;
-        }
-
-
-
+        this.rectangle.update(delta);
+        this.rectangle2.update(delta);
 
     }
 
     @Override
     public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
-        graphics.drawRect(this.xRec, this.yRec, 100, 100);
-        graphics.drawString("Hello you!", 50, 50);
-        graphics.drawOval(this.xOv, this.yOv, 50, 20, 100);
-        graphics.drawOval(this.xCi, this.yCi, 100, 100);
+        this.rectangle.render(graphics);
+        this.rectangle2.render(graphics);
     }
 
     public static void main(String[] argv) {
--- src/GamesOO/src/at/grl/games/firstgame/Rectangle.java
+++ src/GamesOO/src/at/grl/games/firstgame/Rectangle.java
@@ -4,15 +4,25 @@ import org.newdawn.slick.Graphics;
 
 public class Rectangle {
     private enum DIRECTION{RIGHT, DOWN, LEFT, UP};
-    private int x;
-    private int y;
+    private float x;
+    private float y;
     private float speed;
 
-    public void render(Graphics graphics){
+    public Rectangle(int x, int y, float speed) {
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+    }
 
+    public void render(Graphics graphics){
+        graphics.drawRect(this.x, this.y, 10, 10);
     }
 
     public void update(int delta){
+        this.x += (float)delta/this.speed;
+        if(this.x >600){
+            this.x = 0;
+        }
 
     }
 }
